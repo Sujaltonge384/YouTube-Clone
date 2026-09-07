1e197f1448567d287d44450a787e0dc816b2c736
Sujal <sujaltonge@gmail.com>
2026-09-08 00:47:01 +0530

    chore: configure MongoDB and backend environment

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,20 @@
 import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+import connectDB from "./config/db.js";
+
+dotenv.config();
 
 const app = express();
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
+// Connect to MongoDB
+connectDB();
 
 // Middleware
+app.use(cors());
 app.use(express.json());
 
 // Test route
