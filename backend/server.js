1b99fed9df0d71792915677e8719853b3fcdcf80
Sujal <sujaltonge@gmail.com>
2026-09-08 00:19:37 +0530

    chore: initialize Express backend with ES modules

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+
+const app = express();
+
+const PORT = 5000;
+
+// Middleware
+app.use(express.json());
+
+// Test route
+app.get("/", (req, res) => {
+  res.json({
+    message: "YouTube Clone API is running",
+  });
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
